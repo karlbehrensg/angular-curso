297184a28eea3f6da8a062a47526cb54b2f7958b
Karl Behrens <karlbehrensg@users.noreply.github.com>
2019-08-27 00:34:09 -0400

    Pipe Slice

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'karl';
+  nombre = 'Karl Behrens';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
 }
