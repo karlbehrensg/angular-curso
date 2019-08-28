5ebd99d2a7c8a9383a7f0ee4e149f2d51cc8d688
Karl Behrens <karlbehrensg@users.noreply.github.com>
2019-08-27 21:24:41 -0400

    Pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -9,4 +9,28 @@ export class AppComponent {
   nombre = 'Karl Behrens';
 
   arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( ( resolve, reject )=>{
+
+    setTimeout( ()=>resolve('Llego la data!'), 3500);
+
+  } )
+
+  fecha = new Date();
 }
