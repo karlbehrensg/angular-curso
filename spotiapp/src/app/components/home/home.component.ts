b86e01c6f9a532165d667312bce1ab51aafdb2c8
Karl Behrens <karlbehrensg@users.noreply.github.com>
2019-09-02 23:54:53 -0400

    HTTPClient - Service

--- spotiapp/src/app/components/home/home.component.ts
+++ spotiapp/src/app/components/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  constructor( private spotify: SpotifyService ) {
 
-  ngOnInit() {
+    this.spotify.getNewReleases();
+
   }
+
 
 }
---
+++ spotiapp/src/app/services/spotify.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) {
+    console.log('Spotify service listo');
+  }
+
+  getNewReleases(){
+
+    const headers = new HttpHeaders({
+    Authorization: 'Bearer BQBqhnQP1c8r-_te5CxVmRZamfyovIJ_O880j3dSIROaNcrSHoggcWb8QdHzXsBAp9-eIHAeWEud0wPeeJU'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+      .subscribe( data => {
+        console.log(data);
+      });
+
+  }
+}
