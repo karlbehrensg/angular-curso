125c61edb8dbff43767f87adf691e9f3cf1713e1
Karl Behrens <karlbehrensg@users.noreply.github.com>
2019-09-03 00:31:15 -0400

    Componente de busqueda

--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -19,4 +19,14 @@ export class SpotifyService {
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers } );
 
   }
+
+  getArtista( termino: string ) {
+
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQBqhnQP1c8r-_te5CxVmRZamfyovIJ_O880j3dSIROaNcrSHoggcWb8QdHzXsBAp9-eIHAeWEud0wPeeJU'
+      });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers } );
+
+  }
 }
